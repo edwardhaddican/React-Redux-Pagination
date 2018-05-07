bddf6c5775183d1bc28813096db1d49f3a263775
Horacio Gutierrez <hogutier@yahoo.com>
2018-05-07 14:30:05 -0500

    start

---
+++ Capstone/optikos-client/simple-react-redux/components/index.js
@@ -0,0 +1,21 @@
+import '@tmkelly28/tk-css'
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import store from './store'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div id='demo' className='fill-xy bg-violet white column center-xy'>
+      <div>
+        <div>Actions&</div>
+        <div>Reducers&</div>
+        <div>Middleware&</div>
+        <div>Redux.</div>
+      </div>
+    </div>
+  </Provider>,
+  document.getElementById('app')
+)
